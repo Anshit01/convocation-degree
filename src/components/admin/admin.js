489d40c55b549efd4f43fada559119f3babd8e2c
Anshit01 <bhardwaj.anshit1379@gmail.com>
2022-12-11 14:02:12 +0530

    image pinning done

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -3,19 +3,60 @@ import "./admin.css";
 import Navbar from "../navbar/navbar";
 import branches from "../../assets/branches.json";
 import parser from "papaparse";
+import {createCanvas, loadImage} from "canvas";
 
 const fileTypes = ["CSV"];
 
 const Admin = () => {
 
-  function generateDegree({name, fatherName, rollNo, cgpi, branch, date}) {
+  const [imgBuffer, setImgBuffer] = useState(undefined);
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [hashes, setHashes] = useState({});
+
+  async function generateDegree({name, fatherName, rollNo, cgpi, branch, date}) {
+    const degree = await loadImage("/degree.jpg");
+    const canvas = createCanvas(degree.width, degree.height);
+	  const ctx = canvas.getContext('2d');
+    ctx.drawImage(degree, 0, 0);
+    ctx.font = '26px Roboto';
+    ctx.fillStyle = '#000';
+    ctx.lineWidth = 2;
     
+    // Set name
+    const nameDimen = ctx.measureText(name);
+    ctx.fillText(name, degree.width / 2 - nameDimen.width / 2 + 140, 620);
+
+    // Set Father's name
+    const fatherNameDimen = ctx.measureText(fatherName);
+    ctx.fillText(fatherName, degree.width / 2 - fatherNameDimen.width / 2 + 140, 677);
+
+    // Set Roll No
+    const rollNoDimen = ctx.measureText(rollNo);
+    ctx.fillText(rollNo, degree.width / 2 - rollNoDimen.width / 2 - 260, 734);
+
+    // Set Branch
+    const branchDimen = ctx.measureText(branch);
+    ctx.fillText(branch, degree.width / 2 - branchDimen.width / 2, 845);
+
+    // Set CGPI
+    const cgpiDimen = ctx.measureText(cgpi);
+    ctx.fillText(cgpi, degree.width / 2 - cgpiDimen.width / 2 + 24, 905);
+
+    // Set Date
+    const dateDimen = ctx.measureText(date);
+    ctx.fillText(date, degree.width / 2 - dateDimen.width / 2 - 310, 1287);
+
+    const base64String = canvas.toDataURL("image/jpeg")
+    setImgBuffer(base64String);
+
+	  return base64String;
   }
 
   const [file, setFile] = useState(null);
   const [dragActive, setDragActive] = useState(false);
   const handleChange = (e) => {
-    console.log("handling file selection")
+    console.log("Handling file selection")
     e.preventDefault();
     e.stopPropagation();
     if (e.target.files && e.target.files[0]) {
@@ -36,7 +77,7 @@ const Admin = () => {
   function handleDrop(e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log("handling drop event")
+    console.log("Handling drop event")
     if (e.dataTransfer.files && e.dataTransfer.files[0]) {
       const filename = e.dataTransfer.files[0].name;
       if (fileTypes.includes(filename.split(".").pop().toUpperCase())) {
@@ -63,10 +104,41 @@ const Admin = () => {
     }
   }
 
-  function generateDegrees(studentData, date, branch) {
-    studentData.forEach((student) => {
-      generateDegree({...student, date, branch});
+  async function generateDegrees(studentData, date, branch) {
+    setTotal(studentData.length);
+    setCount(0)
+    setImgBuffer(undefined)
+    setHashes([])
+    studentData.forEach(async (student) => {
+      const base64Image = await generateDegree({...student, date, branch});
+      const res = await fetch("https://us-central1-memecast-2913f.cloudfunctions.net/pinToIPFS-default", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify({
+          "data": {
+            "base64Image": base64Image,
+            "name": "Degree",
+            "description": "Degree issues by NITH",
+            "category": "Digital Certificate",
+            "password": "degree123&?/qsdrfg"
+          }
+        })
+      })
+      setCount((count) => count + 1)
+      const data = await res.json()
+      setHashes((hashes) => ({...hashes, metadata: data.result.metadata}))
     })
+  }
+
+  async function mint() {
+    if (Object.keys(hashes).length === 0) {
+      alert("Generate degrees first")
+      return
+    }
+    // TODO: Mint NFTs here using hashes array
+
   }
 
   return (
@@ -95,7 +167,6 @@ const Admin = () => {
             >
               {!file || !file.name ? (
                 <>
-                  {/* <AiOutlineCloudUpload /> */}
                   <p>{dragActive? "Drop the file here": "Drag and drop or click here to upload file"}</p>
                 </>
               ) : (
@@ -110,11 +181,15 @@ const Admin = () => {
                 required
               />
             </label>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Generate Degrees" />
           </form>
+          <div className="admin-progress">
+            <p>Progress: {count}/{total}</p>
+          </div>
+          <button className="mint-button" onClick={mint}><b>Create Degrees on the blockchain</b></button>
         </div>
         <div className="admin-right">
-          
+          <img src={imgBuffer? imgBuffer: "/degree.jpg"} height="700px" alt=""/>
         </div>
       </div>
     </>
